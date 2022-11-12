6a7899ed148e415fc71779ba0214de1b9fc72917
Kevin Chen <1354016594@qq.com>
2022-11-12 22:35:42 +0800

    开放上传器源代码实现

---
+++ FileDto.cs
@@ -0,0 +1,72 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TestClient
+{
+    public class CreateFileEntryRequest
+    {
+        /// <summary>
+        /// 文件的路径，以 / 开头，按照unix风格，例如 /home/kcos/pictures/test/
+        /// 结尾的 / 可有可无，后端会自动处理
+        /// 文件长度比较玄学，跟虚拟机、数据库的支持有关系
+        /// 建议每个“段”不要超过255个字符（建议而已，就算超过了我也不知道会发生什么）
+        /// </summary>
+        public string Path { get; set; } = "/";
+
+        /// <summary>
+        /// 带后缀的文件名
+        /// 文件名中不要含有特殊字符，例如空格，/，emoji之类的，建议全程ASCII
+        /// 文件名（含后缀名）不要超过255字节（注意，中文算3字节）
+        /// </summary>
+        public string FileNameWithExt { get; set; } = "file.unknown";
+
+        public long FileSize { get; set; } = 0L;
+
+        /// <summary>
+        /// 后端为了减轻储存压力，会把那些已经存在的数据进行重复引用
+        /// SHA256就是一个很重要的判据
+        /// 当后端发现，文件的SHA256与文件大小完全一致的时候，会自动重复引用
+        /// </summary>
+        [JsonPropertyName("sha256")]
+        public string SHA256 { get; set; } = null!;
+
+        public string MimeType { get; set; } = "application/octet-stream";
+
+        /// <summary>
+        /// 以下两个字段看文档解释
+        /// </summary>
+        public int Protection { get; set; } = 0;
+        public string SecurityPayload { get; set; } = string.Empty;
+
+        public DateTimeOffset DeadLine { get; set; } = DateTimeOffset.MaxValue;
+    }
+
+    public class CreateFileEntryReply
+    {
+        /// <summary>
+        /// 以后下载用ID来下就可以了
+        /// </summary>
+        public long Id { get; set; } = 0L;
+
+        public long FileSize { get; set; } = 0L;
+
+        public long Frames { get; set; } = 0L;
+
+        /// <summary>
+        /// 如果这个字段为0，表示根本不需要进行传送了，已经有可以复用的了
+        /// </summary>
+        public long NextRequestedFrame { get; set; } = 1L;
+
+        public string[] PathHierarchy { get; set; } = Array.Empty<string>();
+
+        public string FileNameWithExt { get; set; } = "file.unknown";
+
+        public DateTimeOffset CreationTime { get; set; } = DateTimeOffset.Now;
+
+        public DateTimeOffset DeadLine { get; set; } = DateTimeOffset.MaxValue;
+    }
+
+    public class UploadFileReply
+    {
+        public long NextRequestedFrame { get; set; } = 0L;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,114 @@
+﻿using System.Buffers;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Security.Cryptography;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using TestClient;
+
+// 准备好所需的Http客户端和必要的Header
+var httpClient = new HttpClient();
+// 这些是假数据
+httpClient.DefaultRequestHeaders.Add("X-AppId", "");
+httpClient.DefaultRequestHeaders.Add("X-AppKey", "");
+httpClient.DefaultRequestHeaders.Add("X-UserId", "");
+var jsonOptions = new JsonSerializerOptions
+{
+    PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+    NumberHandling = JsonNumberHandling.AllowReadingFromString,
+    WriteIndented = false
+};
+
+// 解析typora调用的参数，然后挨个上传、输出
+var files = args;
+var works = files.Select(async i => await UploadSingleFile(i));
+var result = await Task.WhenAll(works);
+
+foreach (var i in result)
+{
+    Console.WriteLine(i);
+}
+
+async ValueTask<string> UploadSingleFile(string filePath)
+{
+    var fileInfo = new FileInfo(filePath);
+    if (!fileInfo.Exists)
+    {
+        throw new FileNotFoundException(filePath);
+    }
+
+    // 计算文件的SHA256，最大化利用已存在的文件
+    using var fileStream = File.OpenRead(filePath);
+    using var sha256 = SHA256.Create();
+    var digest = await sha256.ComputeHashAsync(fileStream);
+
+    // 创建文件句柄
+    var createEntryDto = new CreateFileEntryRequest
+    {
+        Path = "/typora/shared_storage",
+        FileNameWithExt = $"{Guid.NewGuid():N}{Path.GetExtension(filePath).ToLower()}",
+        FileSize = fileInfo.Length,
+        SHA256 = Convert.ToHexString(digest).ToLower(),
+        MimeType = GetMimeType(fileInfo.Name),
+        DeadLine = DateTimeOffset.Now.AddYears(10)
+    };
+    using var createEntryRequest = new HttpRequestMessage(HttpMethod.Post, "https://cos.kevinc.ltd/file/createFileEntry");
+    createEntryRequest.Content = JsonContent.Create(createEntryDto, MediaTypeHeaderValue.Parse("application/json"), jsonOptions);
+    using var createEntryResponse = await httpClient.SendAsync(createEntryRequest);
+    if (!createEntryResponse.IsSuccessStatusCode)
+    {
+        return "Error!";
+    }
+    var fileMetaData = await createEntryResponse.Content.ReadFromJsonAsync<CreateFileEntryReply>(jsonOptions);
+
+    if (fileMetaData!.NextRequestedFrame == 0)
+    {
+        // 已经出现过的文件，不需要再传了
+        Console.WriteLine("Duplicated file reused.");
+        return $"https://cos.kevinc.ltd/file/download?fileId={fileMetaData.Id}";
+    }
+
+    // 将文件变成1MB的大小的分块上传
+    // 这是一个非常耗费IO的任务，如果我们用数组做buffer的话，难免会出现大量内存分配，引发GC问题
+    using var memoryHolder = MemoryPool<byte>.Shared.Rent(1048576);
+    for (long i = 0; i < fileMetaData.Frames; i++)
+    {
+        var offset = i * 1048576;
+        fileStream.Seek(offset, SeekOrigin.Begin);
+        var contentLength = await fileStream.ReadAsync(memoryHolder.Memory);
+
+        // 组装一个Http请求
+        using var request = new HttpRequestMessage(HttpMethod.Put, $"https://cos.kevinc.ltd/file/upload?fileId={fileMetaData.Id}&seqNumber={i + 1}");
+        request.Content = new ReadOnlyMemoryContent(memoryHolder.Memory[..contentLength]);
+        using var resp = await httpClient.SendAsync(request);
+
+        if (!resp.IsSuccessStatusCode)
+        {
+            return "Error!";
+        }
+        else
+        {
+            // 输出上传分包的进度
+            Console.WriteLine($"Writing file pack {i + 1} out of {fileMetaData.Frames}");
+        }
+    }
+
+    return $"https://cos.kevinc.ltd/file/download?fileId={fileMetaData.Id}";
+}
+
+// 根据文件后缀名匹配mime-type，这里的并不完善，也不安全
+string GetMimeType(string fileName)
+{
+    var ext = Path.GetExtension(fileName).ToLower();
+    return ext switch
+    {
+        ".mp4" => "video/mp4",
+        ".mp3" => "audio/mp3",
+        ".aac" => "audio/aac",
+        ".png" => "image/png",
+        ".webp" => "image/webp",
+        ".jpg" or ".jpeg" => "image/jpeg",
+        ".pdf" => "application/pdf",
+        _ => "application/octet-stream",
+    };
+}
